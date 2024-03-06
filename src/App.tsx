7f7cac99690e5108153c635c50edce778defc253
hyeon9782 <hyeon9782@gmail.com>
2024-03-06 22:51:04 +0900

    feat: jotai, vanilla extract 의존성 추가 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import "./styles/app.css";
 function App() {
   return (
     <>
