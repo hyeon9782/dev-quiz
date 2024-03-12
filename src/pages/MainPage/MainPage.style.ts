91dc2128415f212a675b42d2241c1b959de78cc2
hyeon9782 <hyeon9782@gmail.com>
2024-03-12 22:45:38 +0900

    feat: MainPage 마크업

---
+++ src/pages/MainPage/MainPage.style.ts
@@ -0,0 +1,29 @@
+import { css } from "@emotion/react";
+
+export const containerStyle = css({
+  width: "800px",
+});
+
+export const titleStyle = css({
+  fontSize: "3rem",
+  textAlign: "center",
+});
+
+export const listStyle = css({
+  marginTop: "100px",
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+export const cardStyle = css({
+  borderRadius: "15px",
+  backgroundColor: "lightgray",
+  fontSize: "3rem",
+  fontWeight: "bold",
+  width: "200px",
+  height: "200px",
+  padding: "15px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,33 @@
+import { useNavigate } from "react-router-dom";
 import Container from "../../components/common/Container/Container";
+import {
+  cardStyle,
+  containerStyle,
+  listStyle,
+  titleStyle,
+} from "./MainPage.style";
 
+// 추후에 디자인 시스템 Card 컴포넌트로 전환
 const MainPage = () => {
-  return <Container>MainPage</Container>;
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <div css={containerStyle}>
+        <h1 css={titleStyle}>환영합니다! 현정호님!</h1>
+        <div css={listStyle}>
+          <div onClick={() => navigate("/practice")} css={cardStyle}>
+            연습하기
+          </div>
+          <div onClick={() => navigate("/room")} css={cardStyle}>
+            게임하기
+          </div>
+          <div onClick={() => navigate("/create")} css={cardStyle}>
+            문제생성
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default MainPage;
